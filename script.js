f3997ddf491938630b4a25f3ef6dd4000bf79f99
ekvedaraite <evelinakvedaraite2003@gmail.com>
2023-11-28 13:11:45 +0200

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+function name() {
+    var element = document.getElementById("name")
+
+    element.style.fontSize = "4em"
+    element.style.color = "red"
+    element.style.textAlign = "center"
+    element.style.position = "absolute"
+    element.style.top = "50%"
+    element.style.left = "50%"
+    element.style.transform = "translate(-50%, -50%)"
+}
